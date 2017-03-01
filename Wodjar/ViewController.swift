9f9d5526351d234404e1d86b87ef9b1fbbb598b4
Bogdan Costea <Bogdan@xtm-skylakes-imac.local>
2017-03-01 16:55:19 +0200

    Initial Commit

---
+++ Wodjar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wodjar
+//
+//  Created by Bogdan on 01/03/17.
+//  Copyright © 2017 X2Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
