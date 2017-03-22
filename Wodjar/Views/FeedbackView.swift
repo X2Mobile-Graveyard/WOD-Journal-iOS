0b49be03299d16d7adb93c05234bd6040d723c77
Mihai Erős <mihai.eros@x2mobile.net>
2017-03-22 14:00:41 +0200

    created FeedbackView

---
+++ Wodjar/Views/FeedbackView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedbackView.swift
+//  Wodjar
+//
+//  Created by Mihai Erős on 21/03/2017.
+//  Copyright © 2017 X2Mobile. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
